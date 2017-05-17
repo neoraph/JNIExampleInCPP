f8bfcf8c9263cb6d83798ba1406321a2e6751813
neoraph <raphael.fromont@sbibits.com>
2017-05-17 16:36:36 +0900

    re arrange repository

--- helloWorldJNI/c++/HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include <jni_md.h>
-#include <iostream>
-#include "HelloWorld.h"
-
-JNIEXPORT void JNICALL Java_HelloWorld_display (JNIEnv *, jobject) {
-	std::cout << " C++ says HelloWorld!" << std::endl ;
-}
-
